ba7f8ceaf83fe100a0018e50fdcc801b332a13f9
Vineya864 <57290095+Vineya864@users.noreply.github.com>
2021-04-30 16:59:04 +0100

    Update signUp.php

--- 190063731_cs2410CourseWork/signUp.php
+++ 190063731_cs2410CourseWork/signUp.php
@@ -13,13 +13,15 @@ session_start();
 		$email=filter_var($_REQUEST['email'], FILTER_SANITIZE_STRING);
 		$emailFinal=$userModel->processEmail($email);
 		$name=filter_var($_REQUEST['Name'], FILTER_SANITIZE_STRING);
-		if($userModel->addUser($emailFinal,password_hash($_REQUEST['pass'], PASSWORD_DEFAULT),$name,0))
+		if($userModel->addUser($emailFinal,password_hash($_REQUEST['pass'], PASSWORD_DEFAULT),$name,0)){
 		$ProcessEmail=$userModel->processEmail($email);	 
 		 $_SESSION['login'] = $ProcessEmail; //write login to server storage
 				 
+					 
 					$url='allAnimals.php';
 					
-					echo '<META HTTP-EQUIV=REFRESH CONTENT="1; '.$url.'">';
+					echo '<META HTTP-EQUIV=REFRESH CONTENT="1; '.$url.'">';}
+                else{}
 		}else{
 			echo "password is not strong enough must contain: UpperCase Letter, Lowercase Letter, and be 4-20 characters long"; 
 			
